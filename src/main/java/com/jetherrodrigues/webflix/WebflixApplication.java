93775702b1ca32e1def433a8e39110f5913df175
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2018-06-22 00:13:46 -0300

    webflix api created

---
+++ src/main/java/com/jetherrodrigues/webflix/WebflixApplication.java
@@ -0,0 +1,12 @@
+package com.jetherrodrigues.webflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebflixApplication.class, args);
+	}
+}
